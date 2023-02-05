40b292389d0fb2274c59037c28aebb0718b69fd9
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-02-05 23:25:30 +0330

    2023-2-5_1401-11-16

---
+++ OnlineShop/Controllers/ShopController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using OnlineShop.Migrations;
+using OnlineShop.Models;
+using OnlineShop.ViewModels;
+
+namespace OnlineShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly DatabaseContext _context;
+        public ShopController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> AddShopping(ShoppingViewModel shopping)
+        {
+            var user = await _context.Users.FindAsync(shopping.UserId);
+
+            if (user == null) return RedirectToAction("Index", "Home");
+
+            //user!=null
+            var product = await _context.Products.FindAsync(shopping.ProductId);
+
+            if (product == null) return RedirectToAction("Index", "Home");
+
+            //product!=null
+            var price = product.Price;
+            var sellOff = product.SellOff;
+            var count = shopping.Count;
+            var finaPrice = price - price * sellOff / 100;
+
+            var factor = await _context.Factors
+                        .FirstOrDefaultAsync(f => f.UserId == shopping.UserId
+                                                    && f.IsPay == false);
+
+            if (factor != null)
+            {
+                var detail = await _context.FactorDetails
+                            .FirstOrDefaultAsync(d => d.FactorId == factor.Id);
+
+                if (detail != null)
+                {
+                    count = detail.DetailCount + count;
+
+                    detail.DetailCount = count;
+                    detail.FinalPrice = (int)finaPrice * count;
+                    detail.Des = shopping.Des;
+
+                    //await _context.SaveChangesAsync();
+                    //return Redirect();
+                }
+
+                //detail == null
+                FactorDetail newDetail = new FactorDetail()
+                {
+                    Id = Guid.NewGuid(),
+                    FactorId = factor.Id,
+                    ProductId = product.Id,
+                    DetailCount = count,
+                    FinalPrice = (int)finaPrice * count,
+                    Des = shopping.Des
+                };
+
+                await _context.FactorDetails.AddAsync(newDetail);
+                //await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+    }
+}
---
+++ OnlineShop/ViewModels/ShoppingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.ViewModels
+{
+    public class ShoppingViewModel
+    {
+        public int ProductId { get; set; }
+        public Guid UserId { get; set; }
+        public int Count { get; set; }
+
+        public string? Des { get; set; }
+    }
+}
