f28ade0bfd6b394945d1beb72a47b194e9f1df15
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-05-28 23:36:47 +0330

    2023-5-28_1401-3-8

---
+++ OnlineShop/Controllers/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+
+namespace OnlineShop.Controllers;
+
+[Authorize]
+public class PaymentController : Controller
+{
+    private readonly DatabaseContext _context;
+
+    public PaymentController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpPost]
+    public IActionResult Pay()
+    {
+        var userId = _context.Users.FirstOrDefault(u => u.Mobile == User.Identity.Name).Id;
+
+        var userFactor = _context.Factors.FirstOrDefault(f => f.UserId == userId && f.IsPay == false);
+
+        if (userFactor == null)
+            return RedirectToAction("shoppingCart", "profile");
+
+
+        int amount=userFactor.TotalPrice;
+        string[] metadata = { "", "" };
+        string description = "خرید از فروشگاه آنلاین";
+
+        return View();
+    }
+
+
+    public IActionResult VerifyPay()
+    {
+        return View();
+    }
+}
